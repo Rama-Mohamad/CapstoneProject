e44ee924b239f36ed32e8ffd4481e00327461d02
Rama.Mohamad <ramamohameed9@gmail.com>
2026-05-22 23:06:48 +0300

    Delete Rama directory

--- Rama/QRCode/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import generateQRCode,read_QRCode_url
-urlpatterns = [
-    path('generate_qr_code/', generateQRCode),
-    path('read_qr_code_url/<str:url_token_numbers>', read_QRCode_url)
-    
-]
